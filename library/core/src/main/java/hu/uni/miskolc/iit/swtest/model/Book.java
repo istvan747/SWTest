d55eb573cc6161e5ec0e91a2b5204abe8cf78816
Balogh Istvan <istvan747@gmail.com>
2018-09-23 09:59:02 +0200

    Book.java was created in core modul.

---
+++ library/core/src/main/java/hu/uni/miskolc/iit/swtest/model/Book.java
@@ -0,0 +1,72 @@
+package hu.uni.miskolc.iit.swtest.model;
+
+import java.util.Date;
+
+public class Book {
+	private int id;
+	private String title;
+	private String author;
+	private Genre genre;
+	private Date released;
+	private boolean rented;
+	
+	public Book(int id, String title, String author, Genre genre, Date released, boolean rented) {
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.genre = genre;
+		this.released = released;
+		this.rented = rented;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+
+	public Date getReleased() {
+		return released;
+	}
+
+	public void setReleased(Date released) {
+		this.released = released;
+	}
+
+	public boolean isRented() {
+		return rented;
+	}
+
+	public void setRented(boolean rented) {
+		this.rented = rented;
+	}
+	
+	
+	
+}
