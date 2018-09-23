1af6dcd811fec8475c0a5f8b6318f681de994c4f
Balogh Istvan <istvan747@gmail.com>
2018-09-23 17:34:26 +0200

    Equals and HashCode methods generated in Book.java.

--- library/core/src/main/java/hu/uni/miskolc/iit/swtest/model/Book.java
+++ library/core/src/main/java/hu/uni/miskolc/iit/swtest/model/Book.java
@@ -65,6 +65,49 @@ public class Book {
 	public void setRented(boolean rented) {
 		this.rented = rented;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((genre == null) ? 0 : genre.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((released == null) ? 0 : released.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (genre != other.genre)
+			return false;
+		if (id != other.id)
+			return false;
+		if (released == null) {
+			if (other.released != null)
+				return false;
+		} else if (!released.equals(other.released))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
 	
 	
 	
