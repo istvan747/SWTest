cee973fd9eedc64c531c23d9f5d1dcee3676ba41
Balogh Istvan <istvan747@gmail.com>
2018-09-23 11:50:09 +0200

    BookAlreadyExistsException.java, BookDoesNotExistsException.java was created.

---
+++ library/core/src/main/java/hu/uni/miskolc/iit/swtest/exceptions/BookDoesNotExistsException.java
@@ -0,0 +1,31 @@
+package hu.uni.miskolc.iit.swtest.exceptions;
+
+public class BookDoesNotExistsException extends Exception{
+
+	public BookDoesNotExistsException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public BookDoesNotExistsException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BookDoesNotExistsException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BookDoesNotExistsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BookDoesNotExistsException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- library/core/src/main/java/hu/uni/miskolc/iit/swtest/service/BookManagerService.java
+++ library/core/src/main/java/hu/uni/miskolc/iit/swtest/service/BookManagerService.java
@@ -2,10 +2,13 @@ package hu.uni.miskolc.iit.swtest.service;
 
 import java.util.Collection;
 
+import hu.uni.miskolc.iit.swtest.exceptions.BookAlreadyExistsException;
+import hu.uni.miskolc.iit.swtest.exceptions.BookDoesNotExistsException;
 import hu.uni.miskolc.iit.swtest.model.Book;
 
 public interface BookManagerService {
-	void recordBook(Book book);	
+	void recordBook(Book book) throws BookAlreadyExistsException;
+	void updateBook(int bookId, Book updatedBook) throws BookDoesNotExistsException;
 	
 	Collection<Book> listBooks();
 	Collection<Book> listRentedBooks();
