f6b746c574b8797550fd1e43fdab19f3b0305bf8
Balogh Istvan <istvan747@gmail.com>
2018-09-27 22:21:44 +0200

    Delete java.util.Date import.

--- library/dao-file/src/main/java/hu/uni/miskolc/iit/swtest/dao/DaoFile.java
+++ library/dao-file/src/main/java/hu/uni/miskolc/iit/swtest/dao/DaoFile.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
